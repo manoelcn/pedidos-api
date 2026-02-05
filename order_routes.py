fbcc9ad5ca55f461e2bbbbeb307ba049aa024952
Manoel <candidomanoel78@gmail.com>
2026-02-05 18:26:58 -0300

    feat: cria rota para cancelar pedido

--- order_routes.py
+++ order_routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from dependencies import get_session
+from dependencies import get_session, verify_token
 from models import Order
 from schemas import OrderSchema
 
-order_router = APIRouter(prefix='/orders', tags=['order'])
+order_router = APIRouter(prefix='/orders', tags=['order'], dependencies=[Depends(verify_token)])
 
 @order_router.get('/')
 async def orders():
@@ -17,3 +17,16 @@ async def create_order(order_schema: OrderSchema, session: Session = Depends(get
     session.add(new_order)
     session.commit()
     return {'message': f'Order {new_order.id} created successfully'}
+
+
+@order_router.post('/cancel/{order_id}')
+async def cancel_order(order_id: int, session: Session = Depends(get_session)):
+    order = session.query(Order).filter(Order.id==order_id).first()
+    if not order:
+        raise HTTPException(status_code=400, detail='order not found')
+    order.status = 'CANCELADO'
+    session.commit()
+    return {
+        'message': f'Order {order.id} cancelled',
+        'order': order
+    }
