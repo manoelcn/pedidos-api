a509d89fdeb3cb7d58784fddf7588f3e822699d7
Manoel <candidomanoel78@gmail.com>
2026-02-06 17:34:34 -0300

    refactor: muda a linha onde o 'order.items' estava

--- order_routes.py
+++ order_routes.py
@@ -61,9 +61,9 @@ async def list_orders(session: Session = Depends(get_session), user: User = Depe
 @order_router.get('/{order_id}/detail')
 async def detail_order(order_id: int, session: Session = Depends(get_session)):
     order = session.query(Order).filter(Order.id==order_id).first()
-    order.items
     if not order:
         raise HTTPException(status_code=400, detail='order not found')
+    order.items
     return {
         'order': order
     }
