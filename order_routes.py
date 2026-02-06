e37db4c075811dd0e30d5d8b94cb110688c546e5
Manoel <candidomanoel78@gmail.com>
2026-02-06 17:44:05 -0300

    feat: remove nível de acesso das rotas

--- order_routes.py
+++ order_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import get_session, verify_token
-from models import Order, User, Item
+from models import Order, Item
 from schemas import OrderSchema, ItemSchema
 
 order_router = APIRouter(prefix='/orders', tags=['order'], dependencies=[Depends(verify_token)])
@@ -20,12 +20,10 @@ async def create_order(order_schema: OrderSchema, session: Session = Depends(get
 
 
 @order_router.post('/cancel/{order_id}')
-async def cancel_order(order_id: int, session: Session = Depends(get_session), user: User = Depends(verify_token)):
+async def cancel_order(order_id: int, session: Session = Depends(get_session)):
     order = session.query(Order).filter(Order.id==order_id).first()
     if not order:
         raise HTTPException(status_code=400, detail='order not found')
-    if not user.admin and user.id != order.id:
-        raise HTTPException(status_code=401, detail='you dont have permission')
     order.status = 'CANCELADO'
     session.commit()
     return {
@@ -48,14 +46,11 @@ async def finalize_order(order_id: int, session: Session = Depends(get_session))
 
 
 @order_router.get('/list')
-async def list_orders(session: Session = Depends(get_session), user: User = Depends(verify_token)):
-    if not user.admin:
-        raise HTTPException(status_code=401, detail='you dont have permission')
-    else:
-        orders = session.query(Order).all()
-        return {
-            'orders': orders
-        }
+async def list_orders(session: Session = Depends(get_session)):
+    orders = session.query(Order).all()
+    return {
+        'orders': orders
+    }
 
 
 @order_router.get('/{order_id}/detail')
